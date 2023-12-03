210d95f99a3114a39f2eecb241a1f1ff7a23f7c5
Julio Rios <jrp8900@gmail.com>
2023-12-03 12:10:16 -0500

    Edit sail alias

--- config/zsh/aliases/laravel.zsh
+++ config/zsh/aliases/laravel.zsh
@@ -1,4 +1,4 @@
 # Laravel
 alias laravelnew="composer create-project laravel/laravel"
-alias sail="./vendor/bin/sail"
+alias sail='[ -f sail ] && sh sail || sh vendor/bin/sail'
 alias php7composer="php7 $(which composer)"
