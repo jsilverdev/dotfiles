1f38079b0cd20b4b32abe75c570c44462b0547c6
Julio Rios <jrp8900@gmail.com>
2024-08-25 01:33:55 -0500

    Improve powershell logic

--- install.ps1
+++ install.ps1
@@ -1,5 +1,3 @@
-$ErrorActionPreference = "Stop"
-
 $CONFIG = "install.win.conf.yaml"
 $DOTBOT_DIR = "lib/dotbot"
 
@@ -10,13 +8,12 @@ Set-Location $BASEDIR
 git -C $DOTBOT_DIR submodule sync --quiet --recursive
 git submodule update --init --recursive $DOTBOT_DIR
 
-foreach ($PYTHON in ('python', 'python3.12')) {
-    # Python redirects to Microsoft Store in Windows 10 when not installed
-    if (& { $ErrorActionPreference = "SilentlyContinue"
-            ![string]::IsNullOrEmpty((&$PYTHON -V))
-            $ErrorActionPreference = "Stop" }) {
-        &$PYTHON $(Join-Path $BASEDIR -ChildPath $DOTBOT_DIR | Join-Path -ChildPath $DOTBOT_BIN) -d $BASEDIR -c $CONFIG $Args
-        return
-    }
+$PYTHON = Get-Command python3* -ErrorAction SilentlyContinue | Select-Object -ExpandProperty Path -First 1
+
+if (![string]::IsNullOrEmpty($PYTHON) -and ![string]::IsNullOrEmpty((&$PYTHON --version))) {
+    $env:PROFILE_LOCATION = $profile.CurrentUserAllHosts
+
+    &$PYTHON $(Join-Path $BASEDIR -ChildPath $DOTBOT_DIR | Join-Path -ChildPath $DOTBOT_BIN) -d $BASEDIR -c $CONFIG $Args
+    return
 }
 Write-Error "Error: Cannot find Python."
