4edf4e0b5af75334aef5bf8b485ae053f5ef68b2
Julio Rios <jrp8900@gmail.com>
2024-10-20 09:49:31 -0500

    Add check winget and install apps

--- install.ps1
+++ install.ps1
@@ -1,3 +1,40 @@
+### Check winget
+try {
+    winget --version > $null
+    Write-Host "winget is installed or enabled" -ForegroundColor Green
+} catch {
+    Write-Host "winget is not installed, please install it" -ForegroundColor Red
+    exit
+}
+
+### Start Installing my apps
+Write-Output "Installing apps...`n"
+$apps = @(
+    "Starship.Starship",
+    "zyedidia.micro",
+    "lsd-rs.lsd",
+    "sharkdp.bat",
+    "Schniz.fnm",
+    "Fastfetch-cli.Fastfetch",
+    "junegunn.fzf",
+    "sharkdp.fd",
+    "dandavison.delta",
+    "Microsoft.VisualStudioCode",
+    "Microsoft.PowerToys"
+)
+
+foreach ($app in $apps) {
+    winget list --id $app -n 1 | Out-Null
+    if (-not $?) {
+        Write-Host "$app is not installed. Installing..." -ForegroundColor Yellow
+        winget install -e --id $app
+    } else {
+        Write-Host "$app is already installed" -ForegroundColor Green
+    }
+}
+Write-Output ""
+### End Installing my apps
+
 ### Start DotBot
 $CONFIG = "install.win.conf.yaml"
 $DOTBOT_DIR = "lib/dotbot"
