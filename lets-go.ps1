470b0acfe48a379f7ee6a348069f25b0009b64fd
Julio Rios <jrp8900@gmail.com>
2024-10-21 18:00:23 -0500

    Logic for lets-go.ps1

---
+++ lets-go.ps1
@@ -0,0 +1,38 @@
+# Check winget and activate
+if($null -eq (Get-Command -Name winget -ErrorAction SilentlyContinue)) {
+    Write-Output "Enable winget..."
+    Add-AppxPackage -RegisterByFamilyName -MainPackage Microsoft.DesktopAppInstaller_8wekyb3d8bbwe
+}
+
+# List apps to install
+$core_apps = @(
+    "Git.Git",
+    "Microsoft.PowerShell"
+)
+
+# For each app, check if not present and install
+foreach ($app in $core_apps) {
+    winget list --id $app -n 1 | Out-Null
+    if (-not $?) {
+        Write-Host "$app is not installed. Installing..." -ForegroundColor Yellow
+        winget install -e --id $app
+    } else {
+        Write-Host "$app is already installed" -ForegroundColor Green
+    }
+}
+
+# If not already set, specify dotfiles destination directory and source repo
+if(!$DOTFILES_DIR) { $DOTFILES_DIR="$HOME\.dotfiles" }
+if(!$DOTFILES_REPO) { $DOTFILES_REPO="https://github.com/jsilverdev/dotfiles.git" }
+
+# Reload PATH
+$env:Path = [System.Environment]::GetEnvironmentVariable("Path","Machine") + ";" + [System.Environment]::GetEnvironmentVariable("Path","User")
+
+if (-not (Test-Path -Path $DOTFILES_DIR -PathType Container)) {
+  New-Item -ItemType Directory -Path "$DOTFILES_DIR" -Force
+  git clone --recursive "$DOTFILES_REPO" "$DOTFILES_DIR"
+}
+
+Set-Location -Path $DOTFILES_DIR
+$installScript = Join-Path $DOTFILES_DIR "install.ps1"
+& pwsh.exe -F $installScript
