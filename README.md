916fea2b484211813ac6b7df8bd2ce1d91ff1d6e
Julio Rios <jrp8900@gmail.com>
2024-11-17 20:19:46 -0500

    Add let-go.sh and edit README

--- README.md
+++ README.md
@@ -1,13 +1,35 @@
-# My dotfiles
+# Dotfiles
 
-## Windows
+Welcome to my dotfiles! This repository contains configurations and scripts to quickly set up a development environment on Windows and Linux.
+
+## Quick Installation
+
+### 🖥️ Windows
+
+To set up your environment on Windows, run the following command in PowerShell:
 
 ```powershell
-Invoke-WebRequest -Uri "https://raw.githubusercontent.com/jsilverdev/dotfiles/refs/heads/main/lets-go.ps1" -OutFile ".\lets-go.ps1"; Set-ExecutionPolicy -Scope CurrentUser -ExecutionPolicy RemoteSigned; .\lets-go.ps1
+Invoke-WebRequest -Uri "https://raw.githubusercontent.com/jsilverdev/dotfiles/master/lets-go.ps1" -OutFile ".\lets-go.ps1"; Set-ExecutionPolicy -Scope CurrentUser -ExecutionPolicy RemoteSigned; .\lets-go.ps1
 ```
 
-## Linux
+### 🐧 Linux
+
+To set up your environment on Linux, use this command:
 
 ```bash
-
+bash <(curl -s https://raw.githubusercontent.com/jsilverdev/dotfiles/master/lets-go.sh)
 ```
+
+## Requirements
+
+### 🖥️ Windows
+- PowerShell 5.0 (This script install Powershell 7)
+- [Dev Mode](https://learn.microsoft.com/en-us/windows/apps/get-started/enable-your-device-for-development) is enabled
+
+### 🐧 Linux
+- Bash 4.0 or newer
+- curl
+
+## Acknowledgments
+
+This dotfiles repository is inspired by [Lissy93's dotfiles](https://github.com/Lissy93/dotfiles).
---
+++ lets-go.sh
@@ -0,0 +1,50 @@
+#!/usr/bin/env bash
+
+core_packages=(
+  'git'
+  'zsh'
+)
+
+function install_debian () {
+  echo -e "${PURPLE}Installing ${1} via apt-get${RESET}"
+  sudo apt install $1
+}
+function install_arch () {
+  echo -e "${PURPLE}Installing ${1} via Pacman${RESET}"
+  sudo pacman -S $1
+}
+
+function multi_system_install () {
+  app=$1
+  if [ -f "/etc/arch-release" ] && hash pacman 2> /dev/null; then
+    install_arch $app # Arch Linux via Pacman
+  elif ! [ -f "/etc/debian_version" ] && hash apt 2> /dev/null; then
+    install_debian $app # Debian via apt-get
+  else
+    echo -e "${YELLOW}Skipping ${app}, as couldn't detect system type ${RESET}"
+  fi
+}
+
+# If not already set, specify dotfiles destination directory and source repo
+DOTFILES_DIR="${DOTFILES_DIR:-$HOME/.dotfiles}"
+DOTFILES_REPO="${DOTFILES_REPO:-https://github.com/jsilverdev/dotfiles.git}"
+
+# For each app, check if not present and install
+for app in ${core_packages[@]}; do
+  if ! hash "${app}" 2> /dev/null; then
+    multi_system_install $app
+  else
+    echo -e "${YELLOW}${app} is already installed, skipping${RESET}"
+  fi
+done
+
+# If dotfiles not yet present then clone
+if [[ ! -d "$DOTFILES_DIR" ]]; then
+  mkdir -p "${DOTFILES_DIR}" && \
+  git clone --recursive ${DOTFILES_REPO} ${DOTFILES_DIR}
+fi
+
+# Execute setup or update script
+cd "${DOTFILES_DIR}" && \
+chmod +x ./install.sh && \
+./install.sh
