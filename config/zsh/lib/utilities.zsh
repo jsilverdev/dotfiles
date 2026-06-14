37d63c7ee2156868b0da0aa8c9876e3760fd76a6
Julio Rios <jrp8900@gmail.com>
2026-06-14 11:05:07 -0500

    Add pnpm to path

--- config/zsh/lib/utilities.zsh
+++ config/zsh/lib/utilities.zsh
@@ -97,3 +97,10 @@ eval "$(starship init zsh)"
 
 ### mise-en-place
 hash "mise" 2> /dev/null && eval "$(mise activate zsh)"
+
+### pnpm
+export PNPM_HOME="$HOME/.local/share/pnpm"
+case ":$PATH:" in
+ *":$PNPM_HOME/bin:"*) ;;
+ *) export PATH="$PNPM_HOME/bin:$PATH" ;;
+esac
